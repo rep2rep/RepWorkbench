4855703ed23c4dcd08eb0ebc334f99ed8705c396
Aaron Stockdill <aaronstockdill@me.com>
2021-11-10 15:41:52 +0000

    Switch to esbuild

---
+++ bundle.js
@@ -0,0 +1,20 @@
+import * as fs from "fs";
+import * as cp from "child_process";
+import * as esbuild from "esbuild";
+
+const { readdirSync } = fs;
+
+import * as path from "path";
+
+let src = path.join(path.resolve("."), "src");
+let dist = path.join(path.resolve("."), "dist");
+
+let targets = [path.join(src, "App.bs.js")];
+
+esbuild.build({
+  entryPoints: targets,
+  bundle: true,
+  minifySyntax: true,
+    sourcemap: true,
+    outfile: path.join(dist, "main.js"),
+});
