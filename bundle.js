8dd9505748936928016d47ea0a4d34214e151be7
Aaron Stockdill <aaronstockdill@me.com>
2022-01-31 14:08:54 +0000

    Minify better

--- bundle.js
+++ bundle.js
@@ -14,7 +14,7 @@ let targets = [path.join(src, "App.bs.js")];
 esbuild.build({
   entryPoints: targets,
   bundle: true,
-  minifySyntax: true,
-    sourcemap: true,
-    outfile: path.join(dist, "main.js"),
+  minify: true,
+  sourcemap: true,
+  outfile: path.join(dist, "main.js"),
 });
