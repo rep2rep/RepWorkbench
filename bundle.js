3c2562181a552df1aa408fcec9695380cc7854cd
Aaron Stockdill <aaronstockdill@me.com>
2022-06-21 14:56:44 +0100

    Try and reduce React duplication

--- bundle.js
+++ bundle.js
@@ -1,33 +1,56 @@
 import * as fs from "fs";
-import * as cp from "child_process";
+import * as path from "path";
 import * as esbuild from "esbuild";
 
 const { readdirSync } = fs;
 
-import * as path from "path";
-
 let src = path.join(path.resolve("."), "src");
 let dist = path.join(path.resolve("."), "dist");
 
-const mode = "debug";
+const mode = process.env.NODE_ENV || "development";
+console.log("Running as " + mode);
 
 const cfg = {
   bundle: true,
-  minify: mode === "release",
-  sourcemap: mode !== "release",
+  sourcemap: mode !== "production",
   target: 'es2020',
-  format: 'iife',
   treeShaking: true,
 };
 
-esbuild.build({
-  ...cfg,
-  entryPoints: [path.join(src, "App.bs.js")],
-  outfile: path.join(dist, "main.js"),
-});
-
-esbuild.build({
-  ...cfg,
-  entryPoints: [path.join(src, "Intelligence.bs.js")],
-  outfile: path.join(dist, "worker.js"),
-})
+if (process.argv[2] === "bundle") {
+  console.log("Bundling");
+  esbuild.build({
+    ...cfg,
+    minify: false,
+    format: 'cjs',
+    external: ["react", "react-dom"],
+    entryPoints: [path.join(src, "App.bs.js")],
+    outfile: path.join(dist, "main.js"),
+  });
+  esbuild.build({
+    ...cfg,
+    minify: false,
+    format: 'cjs',
+    external: ["react", "react-dom"],
+    entryPoints: [path.join(src, "Intelligence.bs.js")],
+    outfile: path.join(dist, "worker.js"),
+  });
+} else if (process.argv[2] === "minify") {
+  console.log("Minifying");
+  esbuild.build({
+    ...cfg,
+    minify: mode === "production",
+    allowOverwrite: true,
+    format: 'iife',
+    entryPoints: [path.join(dist, "main.js")],
+    outfile: path.join(dist, "main.js"),
+  });
+  esbuild.build({
+    ...cfg,
+    minify: mode === "production",
+    allowOverwrite: true,
+    format: 'iife',
+    entryPoints: [path.join(dist, "worker.js")],
+    outfile: path.join(dist, "worker.js"),
+  });
+}
